dcb396144bb2aa39a874cdd57fdba42477fa0a9f
Barbara <barbara.basfer@educa.jcyl.es>
2023-04-19 11:39:21 +0200

    Cambio de archivo desde barbara

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -1,7 +1,16 @@
 package prueba;
 
+import java.util.Scanner;
+
 public class Prueba {
 
-	int valorCualquiera;
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		System.out.print("Escribe un número: ");
+		int entero = sc.nextInt();
+
+	}
 
 }
